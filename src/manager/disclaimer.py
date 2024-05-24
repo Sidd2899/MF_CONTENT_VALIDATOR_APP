7ef3fddaedc8d805fa19fe51e663d6bd2fcbe3a3
msumit2000 <sumit.mahanwar@dataeaze.io>
2024-05-24 16:49:14 +0530

    changes as per psql

--- src/manager/disclaimer.py
+++ src/manager/disclaimer.py
@@ -1,17 +1,16 @@
 # manager/disclaimer.py
-
-import mysql.connector
+import psycopg2
 from src.manager.rules import Rules
 from datetime import datetime
 from src.config.queries import INSERT_DISCLAIMER, SELECT_DISCLAIMERS, UPDATE_DISCLAIMER, DELETE_DISCLAIMER
 from src.config.credentials import db_config
 
 try:
-        db = mysql.connector.connect(**db_config)
-        cursor = db.cursor()
-except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        exit()
+    conn = psycopg2.connect(**db_config)
+    cursor = conn.cursor()
+except Exception as error:
+    print(f"Error connecting to PostgreSQL: {error}")
+    exit()
 
 class Disclaimer(Rules):
     def __init__(self, name_of_disclaimer, rule_id, actual_disclaimer):
@@ -24,35 +23,38 @@ class Disclaimer(Rules):
             now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             values = (self.rule_id, self.actual_disclaimer, now, now)
             cursor.execute(INSERT_DISCLAIMER, values)
-            db.commit()
-            return "Disclaimer added successfully!"
-        except mysql.connector.Error as err:
-            return f"Error: {err}"
+            conn.commit()
+            # return "Disclaimer added successfully!"
+            return 1
+        except Exception as error:
+            return f"Error : {error}"
 
     @staticmethod
     def list_disclaimers():
         try:
             cursor.execute(SELECT_DISCLAIMERS)
             disclaimers = cursor.fetchall()
-            return disclaimers
-        except mysql.connector.Error as err:
-            return f"Error: {err}"
+            return 1, disclaimers
+        except Exception as error:
+            return 2,f"Error : {error}"
 
     def edit_disclaimer(self, disclaimer_id, new_rule_id, new_actual_disclaimer):
         try:
             now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             values = (new_rule_id, new_actual_disclaimer, now, disclaimer_id)
             cursor.execute(UPDATE_DISCLAIMER, values)
-            db.commit()
-            return "Disclaimer updated successfully!"
-        except mysql.connector.Error as err:
-            return f"Error: {err}"
+            conn.commit()
+            # return "Disclaimer updated successfully!"
+            return 1
+        except Exception as error:
+            return f"Error : {error}"
 
     def delete_disclaimer(self, disclaimer_id):
         try:
             cursor.execute(DELETE_DISCLAIMER, (disclaimer_id,))
-            db.commit()
-            return "Disclaimer deleted successfully!"
-        except mysql.connector.Error as err:
-            return f"Error: {err}"
+            conn.commit()
+            # return "Disclaimer deleted successfully!"
+            return 1
+        except Exception as error:
+            return f"Error : {error}"
 
