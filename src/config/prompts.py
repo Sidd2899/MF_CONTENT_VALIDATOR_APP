52a44403efe4256b42e79928e608c601ff681a73
msumit2000 <sumit.mahanwar@dataeaze.io>
2024-05-26 16:02:04 +0530

    added new prompt

--- src/config/prompts.py
+++ src/config/prompts.py
@@ -1,16 +1,15 @@
 
-PROMPT = '''Please analyze the provided document and return response each time similar word is found.
-            Please give us only answers and answer in short do not explain.
-            check if the exact terms "Riskometer", "Mutual Fund investments are subject to market risks" appears in the text.
-            Check  Low, Low to Moderate, Moderate, Moderately High, High, Very High like word are present if not present or not mentioned then return only No.
-            Check fund type i.e. open-ended or close-ended or any other if not present or not mentioned then return only No.
-            Give all the name of the fund manager, or Managing Director if not present or not mentioned then return only No.
-            The output in response will be key:value pair as following.
-            Riskometer : Yes/No
-            Mutual Fund investments are subject to market risks : Yes/No
-            Risk : return risk type like Low, Low to Moderate, Moderate, Moderately High, High, Very High
-            Fund Type : answer
-            Fund Manager : Details
+PROMPT = '''Task: Analyze the provided document text and provide the following details.
+            1. Check if the following  exact terms are appeares in text: 
+                ["Riskometer", "Mutual Fund investments are subject to market risks" appears in the text.]
+                If given text appears then return "Yes" otherwise return "No".
+            2. Check  Type of Risk i.e "Low", "Low to Moderate", "Moderate", "Moderately High", "High", "Very High" like word are present.
+            3. Check fund type i.e. open-ended or close-ended or any other. 
+            4. Give all the name of the fund manager, or Managing Director.
+            5. If any details/ terms are  not present or not mentioned then stictly return "No" in output.
+            6. Output Format: Provide the output in pure JSON format, adhering to the specified structure:
+               {"Riskometer":"Yes or No", "Mutual Fund investments are subject to market risks": "Yes or No", "Risk": "Enter Risk Type", "Fund Type": "Enter the fund type","Fund Manager" : "Enter Fund Manager Details"}
+
             '''
 
 QUESTIONS = ''' Scheme Riskometer present in text?
