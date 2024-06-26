8c3cda8d2377865447defd5d01479ac94f33334a
msumit2000 <sumit.mahanwar@dataeaze.io>
2024-06-26 15:40:28 +0530

    dynamic logout api

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from starlette.responses import HTMLResponse, JSONResponse, RedirectResponse
 from starlette.status import HTTP_401_UNAUTHORIZED
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.staticfiles import StaticFiles
-from src.config.credentials import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, REDIRECT_URL
+from src.config.credentials import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, REDIRECT_URL, BUILD_PATH
 from src.endpoints import program, validation, user_management
 from src.endpoints import rule_endpoint
-
+from src.mf_user_management import standard_login
+from fastapi import Form, HTTPException, Response
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -27,60 +29,98 @@ oauth.register(
 )
 
 
-
-
 def get_current_user(request: Request):
     '''
     Functinality to access user from session, it will ensure 
     that user need to be in session to access apis
     ''' 
     user = request.session.get('user')
-    if not user:
-        # return RedirectResponse(url='/home')
-    
+    if not user:    
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return user
 
-@app.get("/home")
-def index(request: Request):
-    user = request.session.get('user')
 
-    if user is not None:
-        email = user['email']
-        html = (
-            f'<pre>Email: {email}</pre><br>'
-            '<a href="/login">login</a>'
-        )
-        return HTMLResponse(html)
+# @app.get("/home")
+# def index(request: Request):
+#     user = request.session.get('user')
+#     if user is not None:
+#         return HTMLResponse("<p>You are already logged in. <a href='/logout'>Logout</a></p>")
+#     else:
+#         html = """
+#         <h1>Welcome to the Login Page</h1>
+#         <a href='/google-login'>Login with Google</a><br>
+#         <a href='/standard-login-form'>Login with Username and Password</a>
+#         """
+#         return HTMLResponse(html)
 
-    # Show the login link
-    return HTMLResponse('<a href="/login">login</a>')
+# @app.get("/standard-login-form")
+# def standard_login_form(request: Request):
+#     html = """
+#     <h1>Standard Login</h1>
+#     <form action="/standard-login" method="post">
+#         <label for="email">Email:</label>
+#         <input type="email" id="email" name="email" required><br><br>
+#         <label for="password">Password:</label>
+#         <input type="password" id="password" name="password" required><br><br>
+#         <button type="submit">Login</button>
+#     </form>
+#     """
+#     return HTMLResponse(html)
 
-@app.get("/login")
+@app.get("/google-login")
 async def login(request: Request):
     url = request.url_for('auth')
     return await oauth.google.authorize_redirect(request, url)
 
 @app.get('/auth')
 async def auth(request: Request):
-    try:
-        token = await oauth.google.authorize_access_token(request)       
-    except OAuthError as e:
-        return HTMLResponse(content=str(e))
+    token = await oauth.google.authorize_access_token(request)
     user = token.get('userinfo')
     if user:
         request.session['user'] = dict(user)
+        request.session['login_method'] = 'google'  # Storing the login method
     return RedirectResponse(url='/root')
+
+# @app.get('/google-logout')
+# def logout(request: Request):
+#     if 'user' in request.session:
+#         request.session.pop('user')
+#         request.session.clear()
+#     else:
+#         request.session.clear()
+#     return RedirectResponse(url='/home')
+
+
+class UserLogin(BaseModel):
+    name: str
+    description: str
+
+@app.post("/user-login")
+async def login(request: Request, logIn: UserLogin):
+    user = standard_login(logIn.email, logIn.password)
+    if user==1:
+        request.session['user'] = user
+        request.session['login_method'] = 'standard'
+        return RedirectResponse(url='/root', status_code=303)
+    elif user ==3:
+        raise HTTPException(status_code=401, detail="User Not FOund")
+    else:
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
 
 @app.get('/logout')
 def logout(request: Request):
-    if 'user' in request.session:
-        request.session.pop('user')
-        request.session.clear()
-    else:
-        request.session.clear()
-    return RedirectResponse(url='/home')
+    login_method = request.session.get('login_method')
+    if login_method == 'google':
+        if 'user' in request.session:
+            request.session.pop('user')
+            request.session.clear()
+        else:
+            request.session.clear()
+        return RedirectResponse(url='/home')
 
+    request.session.pop('user', None)  # Remove user data from session
+    request.session.clear()            # Clear all other session data
+    return RedirectResponse(url='/home')
 
 app.include_router(rule_endpoint.router)
 app.include_router(program.router)
@@ -88,26 +128,24 @@ app.include_router(user_management.router)
 app.include_router(validation.router)
 
 
-app.mount("/static", StaticFiles(directory="/home/user/Downloads/build/static"), name="static")
+app.mount("/static", StaticFiles(directory= BUILD_PATH + "/static"), name="static")
+
 
 @app.get("/programtypes")
 async def program_types(request: Request, user=Depends(get_current_user)):
-    return HTMLResponse(content=open("/home/user/Downloads/build/index.html").read())
+    return HTMLResponse(content=open(BUILD_PATH+"/index.html").read())
 
 @app.get("/rules")
 async def rule_endpoint(request: Request, user=Depends(get_current_user)):
-    return HTMLResponse(content=open("/home/user/Downloads/build/index.html").read())
+    return HTMLResponse(content=open(BUILD_PATH+"/index.html").read())
 
 @app.get("/validate-content")
 async def validate_content(request: Request, user=Depends(get_current_user)):
-    return HTMLResponse(content=open("/home/user/Downloads/build/index.html").read())
+    return HTMLResponse(content=open(BUILD_PATH+"/index.html").read())
 
 @app.get("/root")
 async def root(request: Request, user=Depends(get_current_user)):
-    return HTMLResponse(content=open("/home/user/Downloads/build/index.html").read())
-
-    
-
+    return HTMLResponse(content=open(BUILD_PATH+"/index.html").read())
 
 
 
